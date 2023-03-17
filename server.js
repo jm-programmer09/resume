4c4bd0cf0b2854b8c3ea51ef8595a0fa2d842340
jm-programmer09 <jackmellett2009@gmail.com>
2023-03-18 09:19:41 +1000

    working save

--- server.js
+++ server.js
@@ -7,17 +7,13 @@ const PORT = 3000;
 const app = express();
 
 // Server Settings
+app.use(express.static(__dirname + "/public/"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // API Responses
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/src/index.html"));
-});
-// For CSS
-app.get("/public/:file", (req, res) => {
-  const file = req.params.file;
-  res.sendFile(path.join(__dirname, `/public/${file}`));
 });
 
 // Server Startup
