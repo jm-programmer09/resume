e4a5cebeb37ad71d0f838e7744969e7e1c5677f0
jm-programmer09 <jackmellett2009@gmail.com>
2023-03-17 21:49:43 +1000

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+// Packages
+const express = require("express");
+const path = require("path");
+
+// Server variables 
+const PORT = 3000;
+const app = express();
+
+// Server Settings
+app.use(express.static(__dirname, + "/public/"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API Responses
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/src/index.html"));
+});
+
+// Server Startup
+app.listen(PORT);
+module.exports = app;
