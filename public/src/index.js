7b4e000ff257efef30538f0b45fec1426e9f841b
jm-programmer09 <jackmellett2009@gmail.com>
2023-03-18 16:08:29 +1000

    js working for resume transition and css added

--- public/src/index.js
+++ public/src/index.js
@@ -1,7 +1,21 @@
-const move_user = (id) => {
-  const element = document.getElementById(id);
-  element.scrollIntoView({
-    behavior: "smooth",
-    scrollOptions: { duration: 2000 }
-  });
+var currents = "main";
+const changeTo = (newID) => {
+  const current = document.getElementById(currents), news = document.getElementById(newID);
+  const frost = document.getElementById("frost");
+
+  currents = newID;
+  
+  frost.style.display = "block";
+  frost.style.animation = "frost_animation linear 2s forwards";
+
+  const interval = setInterval(function (){
+    current.style.display = "none";
+    news.style.display = "flex";
+    clearInterval(interval);
+  }, 1000);
+
+  const display_interval = setInterval(function (){
+    frost.style.display = "none";
+    clearInterval(display_interval);
+  }, 1999);
 };
